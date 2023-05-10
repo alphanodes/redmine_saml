bc10f9fe2c536c1116e5d2c20672ee75f1c1c58d
Alexander Meindl <a.meindl@alphanodes.com>
2023-05-10 10:23:59 +0200

    Version bump

--- init.rb
+++ init.rb
@@ -12,7 +12,7 @@ Redmine::Plugin.register :redmine_saml do
   requires_redmine version_or_higher: '5.0'
 
   begin
-    requires_redmine_plugin :additionals, version_or_higher: '3.0.8'
+    requires_redmine_plugin :additionals, version_or_higher: '3.0.9'
   rescue Redmine::PluginNotFound
     raise 'Please install additionals plugin (https://github.com/alphanodes/additionals)'
   end
